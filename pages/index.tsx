d1f93d1c728d69215f6dfce226c2d1ce9e635224
DESKTOP-EHSA6PD\User <seeyee@barcove.my>
2022-04-24 20:41:43 +0800

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <main>
+        {/* SideBar */}
+        {/* Center */}
+      </main>
+
+      <div>
+        {/* Player */}
+      </div>
+    </div>
+  )
+}
+
+export default Home
