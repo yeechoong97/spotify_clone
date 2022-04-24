ddd13c019d656ef056ee2a80dc119739da4844d2
DESKTOP-EHSA6PD\User <seeyee@barcove.my>
2022-04-24 21:24:38 +0800

    Built the sidebar component 1/3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import SideBar from '../components/SideBar'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="bg-black h-screen overflow-hidden">
       <main>
-        {/* SideBar */}
+        <SideBar />
         {/* Center */}
       </main>
 
