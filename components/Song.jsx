a161d0d77193cf7d4a794981c526ee40618bc563
Yee Choong <yeechoong97@gmail.com>
2022-05-02 17:11:21 +0800

    Implemented recoil state for playing part

--- components/Song.jsx
+++ components/Song.jsx
@@ -1,12 +1,26 @@
+import { useRecoilState } from "recoil";
 import useSpotify from "../hooks/useSpotify"
 import { millisToMinutesAndSeconds } from "../lib/time"
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 
 const Song = ({ track, order }) => {
 
     const spotifyApi = useSpotify();
+    const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+    const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+
+    const playSong = () => {
+        setCurrentTrackId(track.track.id);
+        setIsPlaying(true);
+        console.log(track.track);
+        spotifyApi.play({
+            uris: [track.track.uri]
+        })
+    }
 
     return (
-        <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg">
+        <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer" onClick={playSong}>
             <div className="flex items-center space-x-4">
                 <p>{order + 1}</p>
                 <img className="h-10 w-10" src={track.track.album.images[0].url} alt="" />
