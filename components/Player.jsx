be09291ee1eabf7f157f469f5d46e25a92b8ba2b
Yee Choong <yeechoong97@gmail.com>
2022-05-02 18:03:33 +0800

    Completed the center part of player component

--- components/Player.jsx
+++ components/Player.jsx
@@ -4,6 +4,8 @@ import { useRecoilState } from "recoil";
 import useSpotify from "../hooks/useSpotify";
 import useSongInfo from "../hooks/useSongInfo";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import { ReplyIcon, SwitchHorizontalIcon } from "@heroicons/react/outline";
+import { PauseIcon, PlayIcon, RewindIcon, FastForwardIcon } from "@heroicons/react/solid";
 
 const Player = () => {
 
@@ -25,6 +27,19 @@ const Player = () => {
         }
     }
 
+    const handlePlayPause = () => {
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+            if (data.body.is_playing) {
+                spotifyApi.pause();
+                setIsPlaying(false);
+            }
+            else {
+                spotifyApi.play();
+                setIsPlaying(true);
+            }
+        })
+    }
+
     useEffect(() => {
         if (spotifyApi.getAccessToken() && !currentTrackId) {
             // fetch the song info
@@ -34,7 +49,7 @@ const Player = () => {
     }, [currentTrackId, spotifyApi, session])
 
     return (
-        <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white  grid-cols-3 text-xs md:text-base px-2 md:px-8">
+        <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
             {/* Left */}
             <div className="flex items-center space-x-4">
                 <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0].url} alt="" />
@@ -44,7 +59,23 @@ const Player = () => {
                 </div>
             </div>
 
+            {/* Center */}
+            <div className="flex items-center justify-evenly">
+                <SwitchHorizontalIcon className="button" />
+                <RewindIcon
+                    // onClick={() => spotifyApi.skipToPrevious()} //API not Working
+                    className="button" />
 
+                {isPlaying ? (
+                    <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+                ) : (<PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+                )}
+
+                <FastForwardIcon
+                    // onClick={() => spotifyApi.skipToNext()} //API not Working
+                    className="button" />
+                <ReplyIcon className="button" />
+            </div>
         </div>
     )
 }
